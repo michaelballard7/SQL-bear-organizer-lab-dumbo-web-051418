2d720acbd559b0fadcdc2e40eec53a09a8f13bd0
Michael Ballard <ballard.michael9@gmail.com>
2018-05-28 14:10:56 +0000

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER,
+  gender INTEGER (0,1),
+  color TEXT,
+  temperament TEXT,
+  alive INTEGER(0,1)
+)
---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES
+("Sandy", 20, "M", "dark brown", "calm", 0);
+
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES
+("Randy", 10, "M", "black", "intense", 1);
+
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES
+("Tabbby", 6, "F", "dark brown", "Nice", 1);
+
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES
+("Sarg", 19, "M", "Green", "Slimy", 0);
+
+INSERT INTO bears(name,age,gender,color,temperament,alive)VALUES
+("Mali", 13, "F", "dark brown", "goofy", 1);
+
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES
+("Grooch", 2, "M", "Black", "Grinchy", 1);
+
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES
+("Wonder", 6, "F", "Blue", "naive", 1);
+
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES
+(null, 20, "M", "black", "aggressive", 0);
